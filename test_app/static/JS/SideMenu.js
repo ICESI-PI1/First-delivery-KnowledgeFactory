9e350e7ef553815d3cf11780ebde9dc93fdd3a6d
JuanEstebanDuque <mijuan79e8@gmail.com>
2023-05-10 15:49:38 -0500

    feat(Side menu) added side menu in the views

---
+++ test_app/static/JS/SideMenu.js
@@ -0,0 +1,44 @@
+iconMenu = document.querySelector('#sideMenu'),
+menu = document.querySelector('#menu'),
+estado = false
+
+function ola(){
+    document.getElementById("sideMenu").style.visibility = "visible";
+}
+
+function countElement(option){
+    var grid = document.querySelectorAll(".gridProducts .card .selectionProject")
+    var count = grid.length
+    if(option == 1){
+        for(i=0; i<count;i++){
+            grid[i].style.width = "0"
+            grid[i].style.height = "0"
+        }
+    }else{
+        for(i=0; i<count;i++){
+            grid[i].style.width = "93%"
+            grid[i].style.height = "93%"
+        }
+    }
+}
+
+window.addEventListener('click', (e) => {
+    if (document.getElementById("sideMenu").contains(e.target)){
+        if(estado == false){
+            menu.classList.toggle('active')
+            document.getElementById("sideMenu").style.visibility = "hidden"
+            document.body.classList.toggle('opacity')
+            countElement(1)
+            estado = true
+        }
+        
+    }else{
+        if(estado){
+            menu.classList.toggle('active')
+            document.body.classList.toggle('opacity')
+            setTimeout(ola, 0300)
+            countElement(2)
+            estado = false;
+        }
+    }
+})
