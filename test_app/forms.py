279d7681827716b09f702602f85bd8cde527e24e
Sebastian Escobar Marin <sebastianescobar17@hotmail.com>
2023-05-09 21:38:37 -0500

    fix(EditProfile.html): fixed the template of the forms

--- test_app/forms.py
+++ test_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 from .models import User, Company
 
 
@@ -13,11 +13,36 @@ class editProfileForm(ModelForm):
     class Meta:
         model=User
         fields=['fullname', 'email', 'phoneNumberU']
+        widgets = {
+            'fullname': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+                }),
+            'email': EmailInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px; padding:left;'
+                }),
+            'phoneNumberU': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+                })
+        }
 
 class editCompanyForm(ModelForm):
     class Meta:
         model=Company
         fields=['address', 'phoneNumberC']
+        widgets = {
+            'address': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+                }),
+            'phoneNumberC': TextInput(attrs={
+                'class': "form-control",
+                'style': 'max-width: 300px;'
+                })
+        }
+
 
 #class registerForm(forms.Form): 
  #   companyNit = forms.CharField(max_length=9, widget = forms.CharField(attrs={'class': 'form-control enter_input'}))
